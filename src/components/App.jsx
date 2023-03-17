92b906f6fbc95be1230f24fba547287961cd8ffb
leroyiq <leroyiq6@gmail.com>
2023-03-17 16:26:35 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Components {
+  state = {};
+  render() {
+    return;
+    <></>;
+  }
+}
